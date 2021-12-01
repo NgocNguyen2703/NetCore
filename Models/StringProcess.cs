b4a5e6374d98a7637005e92f8957a735d6c9e731
NgocNguyen2703 <nguyenthingoc270300@gmail.com>
2021-12-01 21:34:22 +0700

    Auto Id

--- Models/StringProcess.cs
+++ Models/StringProcess.cs
@@ -8,9 +8,9 @@ namespace NETCORE.Models
             string strkey = "";
             string numPart = "", strPart = "";
             numPart = Regex.Match (id, @"\d+").Value;
-            numPart = Regex.Match (id, @"\D+").Value;
+            strPart = Regex.Match (id, @"\D+").Value;
             int intPart = (Convert.ToInt32(numPart) + 1);
-            for(int i = 1; i < numPart.Length - intPart.ToString().Length; i++);
+            for(int i = 0; i < numPart.Length - intPart.ToString().Length; i++)
             {
                 strPart += "0";
             }
